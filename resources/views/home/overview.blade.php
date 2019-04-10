75ddd546ac05a3f65471bb3e0f2d5ca7749c771f
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-04-10 18:02:58 +0200

    Just filling in some stuff, not quite sure what is really ideal here tbh.

--- resources/views/home/overview.blade.php
+++ resources/views/home/overview.blade.php
@@ -5,9 +5,8 @@
         </h5>
         <div class="card-body">
             <div class="card-text">
-                <div class="row">
-                    <h1>Some server stats, roles etc</h1>
-                </div>
+                <h2>{{ __('Your role(s) are :roles', ['roles' => $user->getRoleNames()->implode(', ')]) }}</h2>
+                <h2>{{ __('You have :count game accounts', ['count' => $user->actions()->where('actionable_type', App\Account::class)->count()]) }}</h2>  
             </div>
         </div>
     </div>
