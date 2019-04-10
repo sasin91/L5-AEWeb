cafbfead3aa5242880a29946627082820d40e7a4
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-04-10 18:08:14 +0200

    Added link to home in right nav dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,6 +79,10 @@
                                         @endlink
                                     @endcan
 
+                                    @link(['to' => 'home', 'class' => 'dropdown-item'])
+                                        {{ __('Home') }}
+                                    @endlink
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
