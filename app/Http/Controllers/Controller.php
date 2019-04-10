7a8ffb31c0150bc15d6dfce6ca3e6a7d7f6891cc
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-04-10 17:55:01 +0200

    Defer to default resourceAbilityMap

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,22 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Get the map of resource methods to ability names.
-     *
-     * @return array
-     */
-    protected function resourceAbilityMap()
-    {
-        return [
-            'index' => 'list',
-            'show' => 'view',
-            'create' => 'create',
-            'store' => 'create',
-            'edit' => 'update',
-            'update' => 'update',
-            'destroy' => 'delete',
-        ];
-    }
 }
